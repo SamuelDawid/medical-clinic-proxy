548c74c22903d90c85aeadc25077ff085dde4912
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-09-14 11:02:34 +0200

    project init

---
+++ src/main/java/com/example/medicalclinicproxy/MedicalClinicProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicalclinicproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicProxyApplication.class, args);
+	}
+
+}
