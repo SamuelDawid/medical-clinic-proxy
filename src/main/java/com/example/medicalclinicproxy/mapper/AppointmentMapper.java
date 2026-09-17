39ef6a730a932bbbb44f4467868e0f140e8e58ab
Samuel Dawid <samuelkrzyszpien@gmail.com>
2026-09-17 14:12:01 +0200

    fix: changed from names to id for both doctor and patient

--- src/main/java/com/example/medicalclinicproxy/mapper/AppointmentMapper.java
+++ src/main/java/com/example/medicalclinicproxy/mapper/AppointmentMapper.java
@@ -1,5 +1,6 @@
 package com.example.medicalclinicproxy.mapper;
 
+import com.example.medicalclinicproxy.dto.AppointmentDto;
 import com.example.medicalclinicproxy.dto.CreateAppointmentCommand;
 import com.example.medicalclinicproxy.model.Appointment;
 import org.mapstruct.Mapper;
@@ -7,12 +8,8 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AppointmentMapper {
+    @Mapping(target = "id",ignore = true)
     Appointment toEntity(CreateAppointmentCommand command);
-    @Mapping(target = "doctorName", source = "doctor.user")
-    @Mapping(target = "patientName", source = "patient.user")
+
     AppointmentDto toDto(Appointment appointment);
-    default String fullName(User user){
-        if(user == null){ return  null;}
-        return user.getFirstName() + " " + user.getLastName();
-    }
 }
