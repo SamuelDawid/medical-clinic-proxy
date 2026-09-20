3a4478aaf47fbc10690a3082c27536d7d804c518
Samuel Dawid <samuelkrzyszpien@gmail.com>
2026-09-20 10:54:46 +0200

    feat: added fallback configuration

--- src/main/java/com/example/medicalclinicproxy/client/MedicalClinicClient.java
+++ src/main/java/com/example/medicalclinicproxy/client/MedicalClinicClient.java
@@ -1,14 +1,13 @@
 package com.example.medicalclinicproxy.client;
 
 import com.example.medicalclinicproxy.dto.DoctorDto;
-import com.example.medicalclinicproxy.dto.PageDto;
 import com.example.medicalclinicproxy.dto.PatientDto;
+import com.example.medicalclinicproxy.medicalClinicFeignClientConfig.FeignMedicalClinicConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(value = "medical-clinic",url = "${medical-clinic.api.url}")
+@FeignClient(value = "medical-clinic", url = "${medical-clinic.api.url}", configuration = FeignMedicalClinicConfiguration.class)
 public interface MedicalClinicClient {
 
     @GetMapping("/patients/{id}")
