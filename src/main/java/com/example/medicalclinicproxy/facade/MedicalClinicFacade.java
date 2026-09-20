856e8c1b6287efd0e41e74c6b147208cc4360beb
Samuel Dawid <samuelkrzyszpien@gmail.com>
2026-09-20 14:54:16 +0200

    feat: added getDoctorFromSpecification and fallback

--- src/main/java/com/example/medicalclinicproxy/facade/MedicalClinicFacade.java
+++ src/main/java/com/example/medicalclinicproxy/facade/MedicalClinicFacade.java
@@ -2,16 +2,22 @@ package com.example.medicalclinicproxy.facade;
 
 import com.example.medicalclinicproxy.client.MedicalClinicClient;
 import com.example.medicalclinicproxy.dto.DoctorDto;
+import com.example.medicalclinicproxy.dto.DoctorSummaryDto;
+import com.example.medicalclinicproxy.dto.PageDto;
 import com.example.medicalclinicproxy.dto.PatientDto;
 import com.example.medicalclinicproxy.exceptions.DoctorNotFoundException;
+import com.example.medicalclinicproxy.exceptions.MedicalClinicProxyException;
 import com.example.medicalclinicproxy.exceptions.MedicalClinicUnavailableException;
 import com.example.medicalclinicproxy.exceptions.PatientNotFoundException;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import jakarta.validation.constraints.NotBlank;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
 
 @RequiredArgsConstructor
 @Service
@@ -24,9 +30,26 @@ public class MedicalClinicFacade {
         return clinicClient.getDoctorById(id);
     }
 
-    @CircuitBreaker(name = "medical-clinic", fallbackMethod = "")
+    @CircuitBreaker(name = "medical-clinic", fallbackMethod = "getPatientFallback")
     public PatientDto getPatient(@NonNull Long id) {
         return clinicClient.getPatientById(id);
+    }
+
+    @CircuitBreaker(name = "clinic", fallbackMethod = "getDoctorsFromSpecificSpecialityFallback")
+    public PageDto<DoctorSummaryDto> getDoctorsFromSpecificSpeciality(@NotBlank String speciality, Pageable pageable) {
+        return clinicClient.getDoctorsBySpeciality(speciality, pageable);
+    }
+
+    private PageDto<DoctorSummaryDto> getDoctorsFromSpecificSpecialityFallback(@NotBlank String speciality, Pageable pageable, Throwable throwable) {
+        if (throwable instanceof MedicalClinicProxyException exception) {
+            throw exception;
+        }
+        if (throwable instanceof CallNotPermittedException) {
+            log.error("Circuit breaker OPEN, speciality {}", speciality);
+        } else {
+            log.error("Error getting doctor details, speciality {}", speciality, throwable);
+        }
+        throw new MedicalClinicUnavailableException("Can not get doctor details - service unavailable", HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     private DoctorDto getDoctorFallback(Long id, Throwable throwable) {
