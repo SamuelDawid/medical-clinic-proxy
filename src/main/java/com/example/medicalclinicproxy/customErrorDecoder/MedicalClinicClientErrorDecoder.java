3f7202c4b5288d12a21e6478a0ea5ad543530fb7
Samuel Dawid <samuelkrzyszpien@gmail.com>
2026-09-20 10:56:00 +0200

    chore: formatting

--- src/main/java/com/example/medicalclinicproxy/customErrorDecoder/MedicalClinicClientErrorDecoder.java
+++ src/main/java/com/example/medicalclinicproxy/customErrorDecoder/MedicalClinicClientErrorDecoder.java
@@ -2,8 +2,8 @@ package com.example.medicalclinicproxy.customErrorDecoder;
 
 import com.example.medicalclinicproxy.exceptions.MedicalClinicAuthException;
 import com.example.medicalclinicproxy.exceptions.MedicalClinicProxyBadRequestException;
-import com.example.medicalclinicproxy.exceptions.MedicalClinicProxyException;
-import com.example.medicalclinicproxy.exceptions.ResourceNotFoundexception;
+import com.example.medicalclinicproxy.exceptions.MedicalClinicUnavailableException;
+import com.example.medicalclinicproxy.exceptions.ResourceNotFoundException;
 import feign.FeignException;
 import feign.Response;
 import feign.RetryableException;
@@ -23,11 +23,11 @@ public class MedicalClinicClientErrorDecoder implements ErrorDecoder {
         FeignException feignException = feign.FeignException.errorStatus(methodKey, response);
         log.error("Feign client error. Method: {}, Status: {}, Body: {}",
                 methodKey, status, responseBody);
-        return switch (status){
-            case NOT_FOUND -> new ResourceNotFoundexception(response.request().toString());
-            case BAD_REQUEST,UNPROCESSABLE_CONTENT -> new MedicalClinicProxyBadRequestException(responseBody,status);
-            case UNAUTHORIZED,FORBIDDEN -> new MedicalClinicAuthException(responseBody,status);
-            case BAD_GATEWAY, SERVICE_UNAVAILABLE, GATEWAY_TIMEOUT,TOO_MANY_REQUESTS -> new RetryableException(
+        return switch (status) {
+            case NOT_FOUND -> new ResourceNotFoundException(response.request().toString());
+            case BAD_REQUEST, UNPROCESSABLE_CONTENT -> new MedicalClinicProxyBadRequestException(responseBody, status);
+            case UNAUTHORIZED, FORBIDDEN -> new MedicalClinicAuthException(responseBody, status);
+            case BAD_GATEWAY, SERVICE_UNAVAILABLE, GATEWAY_TIMEOUT, TOO_MANY_REQUESTS -> new RetryableException(
                     response.status(),
                     feignException.getMessage(),
                     response.request().httpMethod(),
@@ -37,8 +37,8 @@ public class MedicalClinicClientErrorDecoder implements ErrorDecoder {
             );
 
             default -> {
-                if(status.value() >= 500){
-                    yield new MedicalClinicProxyException(responseBody,status);
+                if (status.value() >= 500) {
+                    yield new MedicalClinicUnavailableException(responseBody, status);
                 }
                 yield decode(methodKey, response);
             }
