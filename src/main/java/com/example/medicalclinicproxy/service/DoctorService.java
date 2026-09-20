ed2a9675743e941d33bfa91854cfa743e78e06a6
Samuel Dawid <samuelkrzyszpien@gmail.com>
2026-09-20 14:55:05 +0200

    feat: added doctor controller and endpoints

---
+++ src/main/java/com/example/medicalclinicproxy/service/DoctorService.java
@@ -0,0 +1,25 @@
+package com.example.medicalclinicproxy.service;
+
+import com.example.medicalclinicproxy.dto.DoctorSummaryDto;
+import com.example.medicalclinicproxy.dto.PageDto;
+import com.example.medicalclinicproxy.facade.MedicalClinicFacade;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DoctorService {
+    MedicalClinicFacade facade;
+
+    @Transactional(readOnly = true)
+    public PageDto<DoctorSummaryDto> findBySpeciality(@NotBlank String speciality, Pageable pageable){
+        return facade.getDoctorsFromSpecificSpeciality(speciality,pageable);
+    }
+}
